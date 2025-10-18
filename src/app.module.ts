80baf95c3c71ba62c92c2d399daba1efcf2062d3
Tiago Figueiredo <tiagosf.tsf@gmail.com>
2025-10-18 10:53:25 -0300

    prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { UserController } from './user/user.controller';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, DatabaseModule],
   controllers: [UserController],
 })
 export class AppModule {}
